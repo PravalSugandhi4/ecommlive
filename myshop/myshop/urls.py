8541eb874ce2e8f1e90ed5e9e79a8a70ceb43626
Praval Sugandhi <pravalsugandhi@Pravals-MacBook-Air.local>
2025-06-19 13:27:53 -0400

    basic ui

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from shop.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', include('shop.urls')),
+    path('',home, name='home'),
+    path('shop/', include('shop.urls'))
     
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Products
 
-# Create your views here.
 def home(request):
-    """
-    Render the home page of the shop.
-    """
-    return render(request, 'shop/base.html')  # Ensure you have a template at 'shop/home.html'
+    products = Products.objects.all()
+    return render(request, 'shop/homepage.html', {'products': products})
+
+def productdetail(request, myproductid):  
+    product = Products.objects.get(productid=myproductid)
+    return render(request, 'shop/productdetail.html', {'product': product})
+
+def wishlist(request):
+    return render(request,"shop/wishlist.html")
